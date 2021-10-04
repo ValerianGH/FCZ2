6ded041f76f4adc015f52e86d3603570d38ef0d3
Valerian <l.dalmas@etudiant.ecolemelies.fr>
2021-10-04 12:13:28 +0200

    Init
    1

---
+++ Assets/Scripts/LoadingScreen.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingScreen : MonoBehaviour
+{
+    [SerializeField] GameObject loadingScreenPrefab;                                //Récupération du GameObject de l'écran de chargement
+
+    public void LoadSceneAsync()                                                    //Fonction pour charger la scène de jeu en fond pendant qu'on affiche l'écran de chargement
+    {
+        StartCoroutine(LoadingScreenCorountine());                                  //On lance une Coroutine pour l'écran de chargement
+    }
+    private IEnumerator LoadingScreenCoroutine()
+    {
+        var prefab = Instantiate(loadingScreenPrefab);                              //On stocke le préfab de l'écran de chargement
+        DontDestroyOnLoad(prefab);                                                  //On empeche la destruction d' l'écran de chargement lors du changement de scène
+        var sceneLoading = SceneManager.LoadSceneAsync("Platformer");               //On stocke dans la variable sceneLoading, la scène du jeu qui se charge en arrière plan
+        sceneLoading.allowSceneActivation = false;                                  //On interdit à la scène du jeu de se lancer
+        while (sceneLoading.isDone == false)                                        //Tant que la scène de jeu n'est pas chargée
+        {
+            if (sceneLoading.progress >= 0.9f)                                      //Si le chargement de la scène de jeu est terminé
+            {
+                if sceneLoading.allowSceneActivision = true;                        //On autorise la scène du jeu à se lancer
+                prefab.GetComponent<Animator>().SetTrigger("disparapaître");        //On lance l'animation "disparaitre" (fondu) de l'écran de chargement
+            }
+            yield return new WaitForSeconds(1);                                     //On attend une seconde
+        }
+    }
+}
